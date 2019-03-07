5ad883c418d2a7b6099623b704a96cee736f8e52
EdManDev <ed.manigat@gmail.com>
2019-03-07 10:15:50 -0800

    update

--- interface.php
+++ interface.php
@@ -1,7 +1,7 @@
 <?php
     if(!empty($_POST['range'])){
         if($_POST['range']=='all'){
-            echo json_encode(['brand'=>'Toyota ','model'=>'EdMan']);
+            echo json_encode(['brand_value'=>' Toyota.','model_value'=>' EdMan.']);
         }
     }
 ?>
